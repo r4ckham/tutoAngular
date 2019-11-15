ce14931577f7296e0f367619b3cf80faae358cf7
R4ckham <ugo@rougelot.fr>
2019-11-15 04:16:48 +0100

    Ajout faker dans post.spec.ts

--- src/app/post.spec.ts
+++ src/app/post.spec.ts
@@ -1,7 +1,8 @@
 import { Post } from './post';
+import faker from 'faker';
 
 describe('Post', () => {
   it('should create an instance', () => {
-    expect(new Post()).toBeTruthy();
+    expect(new Post(faker.lorem.sentence() , faker.lorem.paragraphs())).toBeTruthy();
   });
 });
